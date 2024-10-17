37d22d3a0ec2ccca37f39c1053b4ce1570a32554
Lei <genleiminimrito.o@gmail.com>
2024-10-18 00:12:59 +0800

    Add project files.

---
+++ Text Messaging Application/Form1.Designer.vb
@@ -0,0 +1,443 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TextMessageApplication
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(TextMessageApplication))
+        Me.lblsend = New System.Windows.Forms.Label()
+        Me.lblrcv = New System.Windows.Forms.Label()
+        Me.txtrcv = New System.Windows.Forms.TextBox()
+        Me.txtsender = New System.Windows.Forms.TextBox()
+        Me.txtmsg = New System.Windows.Forms.TextBox()
+        Me.lblcmp = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Lbldear = New System.Windows.Forms.Label()
+        Me.lblmsg = New System.Windows.Forms.Label()
+        Me.lblsinc = New System.Windows.Forms.Label()
+        Me.lblfrom = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblsend
+        '
+        Me.lblsend.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblsend.AutoSize = True
+        Me.lblsend.Location = New System.Drawing.Point(21, 32)
+        Me.lblsend.Name = "lblsend"
+        Me.lblsend.Size = New System.Drawing.Size(41, 13)
+        Me.lblsend.TabIndex = 0
+        Me.lblsend.Text = "Sender"
+        '
+        'lblrcv
+        '
+        Me.lblrcv.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblrcv.AutoSize = True
+        Me.lblrcv.Location = New System.Drawing.Point(21, 63)
+        Me.lblrcv.Name = "lblrcv"
+        Me.lblrcv.Size = New System.Drawing.Size(50, 13)
+        Me.lblrcv.TabIndex = 1
+        Me.lblrcv.Text = "Receiver"
+        '
+        'txtrcv
+        '
+        Me.txtrcv.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.txtrcv.Location = New System.Drawing.Point(82, 58)
+        Me.txtrcv.Multiline = True
+        Me.txtrcv.Name = "txtrcv"
+        Me.txtrcv.Size = New System.Drawing.Size(236, 27)
+        Me.txtrcv.TabIndex = 3
+        '
+        'txtsender
+        '
+        Me.txtsender.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.txtsender.Location = New System.Drawing.Point(82, 25)
+        Me.txtsender.Multiline = True
+        Me.txtsender.Name = "txtsender"
+        Me.txtsender.Size = New System.Drawing.Size(236, 27)
+        Me.txtsender.TabIndex = 4
+        '
+        'txtmsg
+        '
+        Me.txtmsg.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.txtmsg.Location = New System.Drawing.Point(24, 166)
+        Me.txtmsg.Multiline = True
+        Me.txtmsg.Name = "txtmsg"
+        Me.txtmsg.Size = New System.Drawing.Size(294, 184)
+        Me.txtmsg.TabIndex = 5
+        '
+        'lblcmp
+        '
+        Me.lblcmp.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblcmp.AutoSize = True
+        Me.lblcmp.Location = New System.Drawing.Point(21, 150)
+        Me.lblcmp.Name = "lblcmp"
+        Me.lblcmp.Size = New System.Drawing.Size(51, 13)
+        Me.lblcmp.TabIndex = 6
+        Me.lblcmp.Text = "Compose"
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(157, 367)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(22, 24)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "B"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.Font = New System.Drawing.Font("Microsoft YaHei", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(185, 367)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(22, 24)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "I"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button3.BackColor = System.Drawing.Color.White
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(213, 367)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(22, 24)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "U"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button4.BackColor = System.Drawing.Color.White
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(241, 367)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(22, 24)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "S"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 378)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Text Style:"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 415)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Text Align:"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 446)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(78, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Text Forecolor:"
+        '
+        'Button5
+        '
+        Me.Button5.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button5.BackColor = System.Drawing.Color.White
+        Me.Button5.Location = New System.Drawing.Point(84, 409)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(57, 24)
+        Me.Button5.TabIndex = 14
+        Me.Button5.Text = "Left"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button6.BackColor = System.Drawing.Color.White
+        Me.Button6.Location = New System.Drawing.Point(147, 409)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(57, 24)
+        Me.Button6.TabIndex = 15
+        Me.Button6.Text = "RIght"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button7.BackColor = System.Drawing.Color.White
+        Me.Button7.Location = New System.Drawing.Point(213, 409)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(57, 24)
+        Me.Button7.TabIndex = 16
+        Me.Button7.Text = "Center"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button8.BackColor = System.Drawing.Color.Red
+        Me.Button8.Location = New System.Drawing.Point(164, 440)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(22, 24)
+        Me.Button8.TabIndex = 17
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button9.BackColor = System.Drawing.Color.Yellow
+        Me.Button9.Location = New System.Drawing.Point(192, 439)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(22, 24)
+        Me.Button9.TabIndex = 18
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button10
+        '
+        Me.Button10.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button10.BackColor = System.Drawing.Color.Green
+        Me.Button10.Location = New System.Drawing.Point(248, 439)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(22, 24)
+        Me.Button10.TabIndex = 19
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'Button11
+        '
+        Me.Button11.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button11.BackColor = System.Drawing.Color.Blue
+        Me.Button11.Location = New System.Drawing.Point(220, 440)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(22, 24)
+        Me.Button11.TabIndex = 20
+        Me.Button11.UseVisualStyleBackColor = False
+        '
+        'Button12
+        '
+        Me.Button12.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button12.BackColor = System.Drawing.Color.Blue
+        Me.Button12.Location = New System.Drawing.Point(220, 470)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(22, 24)
+        Me.Button12.TabIndex = 25
+        Me.Button12.UseVisualStyleBackColor = False
+        '
+        'Button13
+        '
+        Me.Button13.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button13.BackColor = System.Drawing.Color.Green
+        Me.Button13.Location = New System.Drawing.Point(248, 469)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(22, 24)
+        Me.Button13.TabIndex = 24
+        Me.Button13.UseVisualStyleBackColor = False
+        '
+        'Button14
+        '
+        Me.Button14.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button14.BackColor = System.Drawing.Color.Yellow
+        Me.Button14.Location = New System.Drawing.Point(192, 469)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(22, 24)
+        Me.Button14.TabIndex = 23
+        Me.Button14.UseVisualStyleBackColor = False
+        '
+        'Button15
+        '
+        Me.Button15.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button15.BackColor = System.Drawing.Color.Red
+        Me.Button15.Location = New System.Drawing.Point(164, 470)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(22, 24)
+        Me.Button15.TabIndex = 22
+        Me.Button15.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(21, 476)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(95, 13)
+        Me.Label4.TabIndex = 21
+        Me.Label4.Text = "Background Color:"
+        '
+        'Button16
+        '
+        Me.Button16.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button16.BackColor = System.Drawing.Color.White
+        Me.Button16.Location = New System.Drawing.Point(417, 16)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(492, 47)
+        Me.Button16.TabIndex = 26
+        Me.Button16.Text = "Send Your Message"
+        Me.Button16.UseVisualStyleBackColor = False
+        '
+        'Lbldear
+        '
+        Me.Lbldear.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Lbldear.AutoSize = True
+        Me.Lbldear.BackColor = System.Drawing.Color.White
+        Me.Lbldear.Location = New System.Drawing.Point(421, 103)
+        Me.Lbldear.Name = "Lbldear"
+        Me.Lbldear.Size = New System.Drawing.Size(36, 13)
+        Me.Lbldear.TabIndex = 27
+        Me.Lbldear.Text = "Dear ,"
+        '
+        'lblmsg
+        '
+        Me.lblmsg.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblmsg.BackColor = System.Drawing.Color.White
+        Me.lblmsg.Location = New System.Drawing.Point(421, 129)
+        Me.lblmsg.Name = "lblmsg"
+        Me.lblmsg.Size = New System.Drawing.Size(487, 333)
+        Me.lblmsg.TabIndex = 28
+        Me.lblmsg.Text = "Your Message"
+        '
+        'lblsinc
+        '
+        Me.lblsinc.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblsinc.AutoSize = True
+        Me.lblsinc.BackColor = System.Drawing.Color.White
+        Me.lblsinc.Location = New System.Drawing.Point(761, 470)
+        Me.lblsinc.Name = "lblsinc"
+        Me.lblsinc.Size = New System.Drawing.Size(47, 13)
+        Me.lblsinc.TabIndex = 29
+        Me.lblsinc.Text = "Sincerly,"
+        '
+        'lblfrom
+        '
+        Me.lblfrom.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblfrom.BackColor = System.Drawing.Color.White
+        Me.lblfrom.Location = New System.Drawing.Point(784, 494)
+        Me.lblfrom.Name = "lblfrom"
+        Me.lblfrom.Size = New System.Drawing.Size(99, 16)
+        Me.lblfrom.TabIndex = 30
+        Me.lblfrom.Text = "               "
+        '
+        'TextMessageApplication
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(919, 587)
+        Me.Controls.Add(Me.lblfrom)
+        Me.Controls.Add(Me.lblsinc)
+        Me.Controls.Add(Me.lblmsg)
+        Me.Controls.Add(Me.Lbldear)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblcmp)
+        Me.Controls.Add(Me.txtmsg)
+        Me.Controls.Add(Me.txtsender)
+        Me.Controls.Add(Me.txtrcv)
+        Me.Controls.Add(Me.lblrcv)
+        Me.Controls.Add(Me.lblsend)
+        Me.Name = "TextMessageApplication"
+        Me.Text = "TextMessageApplication"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblsend As Label
+    Friend WithEvents lblrcv As Label
+    Friend WithEvents txtrcv As TextBox
+    Friend WithEvents txtsender As TextBox
+    Friend WithEvents txtmsg As TextBox
+    Friend WithEvents lblcmp As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Lbldear As Label
+    Friend WithEvents lblmsg As Label
+    Friend WithEvents lblsinc As Label
+    Friend WithEvents lblfrom As Label
+End Class
---
+++ Text Messaging Application/Form1.vb
@@ -0,0 +1,109 @@
+﻿Public Class TextMessageApplication
+    Private Sub TextMessageApplication_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Lbldear.Text = "Dear " & txtrcv.Text & ","
+        lblfrom.Text = txtsender.Text
+        lblmsg.Text = txtmsg.Text
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        lblmsg.Font = New Font(lblmsg.Font, FontStyle.Bold)
+        Lbldear.Font = New Font(Lbldear.Font, FontStyle.Bold)
+        lblfrom.Font = New Font(lblfrom.Font, FontStyle.Bold)
+        lblsinc.Font = New Font(lblsinc.Font, FontStyle.Bold)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        lblmsg.Font = New Font(lblmsg.Font, FontStyle.Italic)
+        Lbldear.Font = New Font(Lbldear.Font, FontStyle.Italic)
+        lblfrom.Font = New Font(lblfrom.Font, FontStyle.Italic)
+        lblsinc.Font = New Font(lblsinc.Font, FontStyle.Italic)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        lblmsg.Font = New Font(lblmsg.Font, FontStyle.Underline)
+        Lbldear.Font = New Font(Lbldear.Font, FontStyle.Underline)
+        lblfrom.Font = New Font(lblfrom.Font, FontStyle.Underline)
+        lblsinc.Font = New Font(lblsinc.Font, FontStyle.Underline)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        lblmsg.Font = New Font(lblmsg.Font, FontStyle.Strikeout)
+        Lbldear.Font = New Font(Lbldear.Font, FontStyle.Strikeout)
+        lblfrom.Font = New Font(lblfrom.Font, FontStyle.Strikeout)
+        lblsinc.Font = New Font(lblsinc.Font, FontStyle.Strikeout)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        lblmsg.TextAlign = ContentAlignment.TopLeft
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        lblmsg.TextAlign = ContentAlignment.TopRight
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        lblmsg.TextAlign = ContentAlignment.TopCenter
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Lbldear.ForeColor = Color.Red
+        lblmsg.ForeColor = Color.Red
+        lblsinc.ForeColor = Color.Red
+        lblfrom.ForeColor = Color.Red
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Lbldear.ForeColor = Color.Yellow
+        lblmsg.ForeColor = Color.Yellow
+        lblsinc.ForeColor = Color.Yellow
+        lblfrom.ForeColor = Color.Yellow
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Lbldear.ForeColor = Color.Blue
+        lblmsg.ForeColor = Color.Blue
+        lblsinc.ForeColor = Color.Blue
+        lblfrom.ForeColor = Color.Blue
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Lbldear.ForeColor = Color.Green
+        lblmsg.ForeColor = Color.Green
+        lblsinc.ForeColor = Color.Green
+        lblfrom.ForeColor = Color.Green
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Lbldear.BackColor = Color.Red
+        lblmsg.BackColor = Color.Red
+        lblsinc.BackColor = Color.Red
+        lblfrom.BackColor = Color.Red
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Lbldear.BackColor = Color.Yellow
+        lblmsg.BackColor = Color.Yellow
+        lblsinc.BackColor = Color.Yellow
+        lblfrom.BackColor = Color.Yellow
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Lbldear.BackColor = Color.Blue
+        lblmsg.BackColor = Color.Blue
+        lblsinc.BackColor = Color.Blue
+        lblfrom.BackColor = Color.Blue
+
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Lbldear.BackColor = Color.Green
+        lblmsg.BackColor = Color.Green
+        lblsinc.BackColor = Color.Green
+        lblfrom.BackColor = Color.Green
+    End Sub
+End Class
